04d3cddbf4ba449ff7294ce2e9a9e0147dbf2c76
jojo051 <youpi051@yahoo.fr>
2021-02-11 17:09:36 +0100

    clean

--- conf.js
+++
@@ -1,8 +0,0 @@
-const  mysql = require('mysql');
-const  connection = mysql.createConnection({
-host :  'localhost', // adresse du serveur
-user :  'me', // le nom d'utilisateur
-password :  'secret', // le mot de passe
-database :  'my_db', // le nom de la base de données
-});
-module.exports = connection;
